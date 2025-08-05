2c9bdc5fe047360a649b7b3c5387fda8720a6029
Mlikota, Marko <mlikota@sas.upenn.edu>
2025-08-05 19:25:49 +0200

    Initialized + Added Existing R-Content

---
+++ R/Templates/RFileTemplate.R
@@ -0,0 +1,30 @@
+# TITLE
+
+# Author, date, contact info
+
+# ------------------------------------------------------------------------------
+
+rm(list = ls())  # delete everything in workspace/environment
+cat("\014")      # delete everything in console
+
+
+# Specify some path:
+
+sDataPath <- "~//Dropbox/Coding/CodingSoftware/R/Templates"
+setwd(sDataPath)
+
+
+# Load some packages:
+
+library(readxl)
+
+
+# ------------------------------------------------------------------------------
+# SOME SECTION
+# ------------------------------------------------------------------------------
+
+# ... some code ...
+
+# ------------------------- some subsection  -------------------------
+
+# ... some code ...
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CodingSoftware
 
+This repository contains useful resources for coding with Julia and with R.
+
+The folder "R" contains
+- folder "Tutorial", with an R-tutorial (written as an .Rmd-file and published as an .html-file) ...
+- folder "Templates", with templates for .R-, .Rmd- and .Rnw-files
+
+The folder "Julia" contains
+- ...
+
+© Marko Mlikota, https://markomlikota.github.io, 5 August 2025
